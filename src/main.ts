6044935e484166ae02bde1b55f042beb7c7ec32b
konthon <konthon@hotmail.co.th>
2025-01-05 05:33:16 +0700

    feat: add express session and create the model in schema

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,29 @@
 import { NestFactory } from '@nestjs/core';
+import { PrismaClient } from '@prisma/client';
+import { PrismaSessionStore } from '@quixo3/prisma-session-store';
+import session from 'express-session';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    session({
+      cookie: {
+        maxAge: 24 * 60 * 60 * 1000, // 24hrs
+      },
+      secret: 'datawow-backend-secret-placeholder',
+      resave: true,
+      saveUninitialized: true,
+      store: new PrismaSessionStore(new PrismaClient(), {
+        checkPeriod: 60 * 60 * 1000, // 1hr
+        dbRecordIdIsSessionId: true,
+        dbRecordIdFunction: undefined,
+      }),
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
