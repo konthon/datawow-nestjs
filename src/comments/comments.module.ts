f7c5b267a5661c556f1c5ce40d5b42d9c9ee9b00
konthon <konthon@hotmail.co.th>
2025-01-05 08:19:13 +0700

    fix: add missing user service provider in comments module

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 
+import { UsersService } from 'src/users/users.service';
+
 import { CommentsResolver } from './comments.resolver';
 import { CommentsService } from './comments.service';
 
 @Module({
-  providers: [CommentsService, CommentsResolver],
+  providers: [CommentsService, CommentsResolver, UsersService],
   exports: [CommentsService],
 })
 export class CommentsModule {}
