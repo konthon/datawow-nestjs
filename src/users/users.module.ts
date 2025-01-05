2068a2918f0e86567ec06f00ce89f5ab0239b1b0
konthon <konthon@hotmail.co.th>
2025-01-05 08:23:49 +0700

    feat: add posts resolve field for users resolver

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 
+import { PostsService } from 'src/posts/posts.service';
+
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
 
 @Module({
-  providers: [UsersService, UsersResolver],
+  providers: [UsersService, UsersResolver, PostsService],
   exports: [UsersService],
 })
 export class UsersModule {}
--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,14 +1,36 @@
-import { Args, Int, Query, Resolver } from '@nestjs/graphql';
+import {
+  Args,
+  Int,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
+
+import { GetPostsArgs } from 'src/posts/dto/get-posts.args';
+import { Post } from 'src/posts/models/post';
+import { PostsService } from 'src/posts/posts.service';
 
 import { User } from './models/user';
 import { UsersService } from './users.service';
 
 @Resolver()
 export class UsersResolver {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly postsService: PostsService,
+  ) {}
 
   @Query(() => User, { nullable: true })
   async user(@Args({ name: 'userId', type: () => Int }) userId: number) {
     return this.usersService.getUser(userId);
+  }
+
+  @ResolveField('posts', () => [Post])
+  async posts(
+    @Parent() user: User,
+    @Args({ nullable: true }) args?: GetPostsArgs,
+  ) {
+    return await this.postsService.getPosts({ ...args, authorId: user.id });
   }
 }
