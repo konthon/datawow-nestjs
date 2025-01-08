83698f842843ae8a63b1fcc2d177fb8609976406
konthon <konthon@hotmail.co.th>
2025-01-08 22:14:59 +0700

    feat: add users service unit test

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaClient } from '@prisma/client';
 import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
 
+import { mockNewUser, mockUsers } from 'src/mockdata/user';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 import { UsersService } from './users.service';
@@ -24,5 +25,35 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getUser', () => {
+    it('should return a user', () => {
+      prismaMock.user.findUnique.mockResolvedValue(mockUsers[0]);
+      expect(service.getUser(1)).resolves.toEqual(mockUsers[0]);
+    });
+    it('should be rejected if user not found', () => {
+      prismaMock.user.findUnique.mockResolvedValue(undefined);
+      expect(service.getUser(0)).rejects.toThrow();
+    });
+  });
+
+  describe('getUserByUsername', () => {
+    it('should return a user', () => {
+      prismaMock.user.findFirst.mockResolvedValue(mockUsers[0]);
+      expect(service.getUserByUsername('first')).resolves.toEqual(mockUsers[0]);
+    });
+  });
+
+  describe('createUser', () => {
+    it('should create a new user', () => {
+      prismaMock.user.create.mockResolvedValue(mockNewUser);
+      expect(
+        service.createUser({ username: mockNewUser['username'] }),
+      ).resolves.toEqual(mockNewUser);
+    });
+    it('should be rejected if no username provided', () => {
+      expect(service.createUser({ username: '' })).rejects.toThrow();
+    });
   });
 });
