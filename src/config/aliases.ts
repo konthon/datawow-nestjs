6180ad9b29da5adfd174e1c1b6d8bff37ecad23d
konthon <konthon@hotmail.co.th>
2025-01-31 04:00:47 +0700

    fix: absolute path with module alias

--- src/config/aliases.ts
+++ src/config/aliases.ts
@@ -1,7 +1,7 @@
 import { addAliases } from 'module-alias';
 import * as path from 'path';
 
-const rootPath = path.resolve(process.cwd(), 'src');
+const rootPath = path.resolve('..', '..', 'src');
 addAliases({
   src: rootPath,
 });
