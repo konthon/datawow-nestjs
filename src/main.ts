256a1819aa3bba9a36e8f60419796f300fa0580e
konthon <konthon@hotmail.co.th>
2025-01-31 03:11:42 +0700

    fix: absolute path with module alias

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,11 @@ import * as cookieParser from 'cookie-parser';
 
 import { AppModule } from './app.module';
 
+if (!process.env.IS_TS_NODE) {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  require('module-alias/register');
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
